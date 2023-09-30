5fbb8f6734ea2cefc91ae58244eac8fb9c3dc394
cristinel08 <cristi.sercaianu33@gmail.com>
2023-09-30 19:38:20 +0300

    StartUp Project

---
+++ build/CMakeFiles/nrf24l01.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cristi/nrf24TestC/NRF24ImageSender/src/RF24.cpp" "/home/cristi/nrf24TestC/NRF24ImageSender/build/CMakeFiles/nrf24l01.dir/src/RF24.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "RF24_NO_INTERRUPT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../{ljetgpio_INCLUDE_DIRS}"
+  "../{OpenCV_INCLUDE_DIRS}"
+  "../inc"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include<RF24.h>
+#include<iostream>
+#include <JetsonGPIO.h>
+#include<time.h>
+#include<string>
+#include<ctime>
+
+//using namespace GPIO; // optional
+
+int main()
+{
+	
+
+//	GPIO::setmode(GPIO::BOARD);
+//	RF24 radio = RF24(15,0);
+//	if(!radio.begin())
+//	{
+//		std::cout << "Nu merge\n";
+//		return 0;
+//	}
+}
